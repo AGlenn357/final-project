92f8411657bb38c38746ef4b9b64eab1ac177aae
Divya Tyagi <tyagi.divya08@gmail.com>
2023-11-21 19:08:34 -0500

    added fstream

--- logo.h
+++ logo.h
@@ -7,13 +7,22 @@ Logo Header File
 
 #pragma once //start of header file
 #include <iostream> //including input output library
+#include <fstream>
 
 using namespace std; //using standard library
 
-void read_logo() {  // main logo displayed at game's start
-    cout << "#######  ####   ####    ####   ##  ##   ###   # \n";
-    cout << "   #    #    #  #####  #       ##  ##   ####  #    \n";
-    cout << "   #    #    #  ####   #  # #  ##  ##   ##  # # \n";
-    cout << "   #     ####   #       ####    ####    ##   ## \n";
-    cout << " \n              THE ULTIMATE RACE\n";
+void read_logo()   // main logo displayed at game's start
+{
+  ifstream txt ("logo.txt", ifstream::in); // read from .txt file
+  char c = txt.get();
+
+  while (txt.good()) {
+    cout << c;
+    c = txt.get();
+  }
+
+  txt.close(); // close .txt file
+
+  cout<<"\n"<<endl; // new line 
+
 }
