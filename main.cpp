b3f3e1255fba5df425eabe52febc56d872bf3478
AGlenn357 <143755656+AGlenn357@users.noreply.github.com>
2023-11-24 22:23:14 -0500

    final result

--- main.cpp
+++ main.cpp
@@ -1,16 +1,18 @@
-#include "Final_Project.h"
-#include "Pilot.h"
-#include "logo.h"
-#include "date_time.h"
-#include "Plane.h"
-#include "CoPilot.h"
-#include "Racing_Game.h"
-#include <iostream>
-#include <string>
-#include <vector>
-#include <ctime>
-#include <cstdlib>
-#include <algorithm>
+/*
+Diperna, Glenn, Tyagi
+AERSP 424
+Final Project
+main.cpp file
+*/
+
+#include "Pilot.h" //include Pilot header file
+#include "Logo.h" //include Logo header file
+#include "Plane.h" //include Plane header file
+#include "CoPilot.h" //include CoPilot header file
+#include "Racing_Game.h" //include Racing_Game header file
+#include <iostream> //include input output library
+#include <string> //include string library
+#include <vector> //include vector library
 
 // function to convert entries to all lowercase for easy recognition
 std::string toLowerCase(const std::string& str) {
@@ -21,87 +23,106 @@ std::string toLowerCase(const std::string& str) {
     return lowerCaseStr;
 }
 
-int main(int argc, char* argv[]) {
-    // Read and display the logo
-    read_logo();
-    date_time();
+//function to output how the play the game
+void directions() {
+    std::cout << "HOW TO PLAY" << std::endl << "Use the s and d keys to move the plane left and right.  For precise lateral movement, use the j and k keys.  Use the c key to speed the plane up, and the d key to slow the plane down.  Press enter after input.  Good luck!" << std::endl << std::endl << std::endl;
+}
+
+int main() {
+    std::system("Clear"); //clear screen
+    read_logo(); //display logo
 
     // Let the player choose their pilot
-    std::string chosenPilotName;
+    int again = 1;
+    std::vector <int> scores;
     while (true) {
-        std::cout << "Enter the name of your Pilot: (Maverick, Iceman, Rooster, Hangman) \n Pilot: ";
-        std::cin >> chosenPilotName;
-        chosenPilotName[0] = toupper(chosenPilotName[0]); // Convert the first letter to uppercase
-
-        // Check if the entered pilot name is valid
-        std::string lowerCasePilotName = toLowerCase(chosenPilotName);
-        if (lowerCasePilotName == "maverick" || lowerCasePilotName == "iceman" || lowerCasePilotName == "rooster" || lowerCasePilotName == "hangman") {
-            break; // Valid input, exit the loop
-        }
-        else {
-            std::cout << "Invalid pilot name. Please choose from Maverick, Iceman, Rooster, or Hangman.\n";
-        }
-    }
-    Pilot playerPilot(chosenPilotName);
-    playerPilot.choosePilot();
-
-    // Let the player choose their plane
-    std::string chosenPlaneModel;
-    while (true) {
-        std::cout << "Enter the model of your Plane: (F-14, F-15ex, F-16, F-18) \n Plane Model: ";
-        std::cin >> chosenPlaneModel;
-        chosenPlaneModel[0] = toupper(chosenPlaneModel[0]); // Convert the first letter to uppercase
-
-        // Check if the entered plane model is valid
-        std::string lowerCasePlaneModel = toLowerCase(chosenPlaneModel);
-        if (lowerCasePlaneModel == "f-14" || lowerCasePlaneModel == "f-15ex" || lowerCasePlaneModel == "f-16" || lowerCasePlaneModel == "f-18") {
-            break; // Valid input, exit the loop
-        }
-        else {
-            std::cout << "Invalid plane model. Please choose from F-14, F-15ex, F-16, or F-18.\n";
-        }
-    }
-
-    Plane playerPlane(chosenPlaneModel);
-    playerPlane.choosePlane();
-
-    // Check if the chosen plane requires a co-pilot
-    if (playerPlane.getRequiresCoPilot()) {
-        // Let the player choose their co-pilot
-        std::string chosenCoPilotName;
-        while (true) {
-            std::cout << "Enter the name of your Co-Pilot: (Goose, Bob) \n Co-Pilot: ";
-            std::cin >> chosenCoPilotName;
-            chosenCoPilotName[0] = toupper(chosenCoPilotName[0]); // Convert the first letter to uppercase
-
-            // Check if the entered co-pilot name is valid
-            std::string lowerCaseCoPilotName = toLowerCase(chosenCoPilotName);
-            if (lowerCaseCoPilotName == "goose" || lowerCaseCoPilotName == "bob") {
+        std::cout << again << std::endl;
+        
+        std::string chosenPilotName; //pilot name
+        while (true) { //loop to get pilot name
+            std::cout << "Enter the name of your Pilot: (Maverick, Iceman, Rooster, Hangman) \n Pilot: ";
+            std::cin >> chosenPilotName;
+            chosenPilotName[0] = toupper(chosenPilotName[0]); // Convert the first letter to uppercase
+            
+            // Check if the entered pilot name is valid
+            std::string lowerCasePilotName = toLowerCase(chosenPilotName);
+            if (lowerCasePilotName == "maverick" || lowerCasePilotName == "iceman" || lowerCasePilotName == "rooster" || lowerCasePilotName == "hangman") {
                 break; // Valid input, exit the loop
             }
             else {
-                std::cout << "Invalid co-pilot name. Please choose from Goose or Bob.\n";
+                std::cout << "Invalid pilot name. Please choose from Maverick, Iceman, Rooster, or Hangman.\n";
             }
         }
-        CoPilot playerCoPilot(chosenCoPilotName);
-        playerCoPilot.chooseCoPilot();
-
-        // Create an instance of RacingGame and start the game with pilot, plane, and co-pilot
+        
+        Pilot playerPilot(chosenPilotName); //create pilot object based on choosen pilot
+        playerPilot.choosePilot(); //output pilot statistics
+        
+        // Let the player choose their plane
+        std::string chosenPlaneModel; //plane
+        while (true) { //loop to get plane
+            std::cout << "Enter the model of your Plane: (F-14, F-15ex, F-16, F-18) \n Plane Model: ";
+            std::cin >> chosenPlaneModel;
+            chosenPlaneModel[0] = toupper(chosenPlaneModel[0]); // Convert the first letter to uppercase
+            
+            // Check if the entered plane model is valid
+            std::string lowerCasePlaneModel = toLowerCase(chosenPlaneModel);
+            if (lowerCasePlaneModel == "f-14" || lowerCasePlaneModel == "f-15ex" || lowerCasePlaneModel == "f-16" || lowerCasePlaneModel == "f-18") {
+                break; // Valid input, exit the loop
+            }
+            else {
+                std::cout << "Invalid plane model. Please choose from F-14, F-15ex, F-16, or F-18.\n";
+            }
+        }
+        
+        Plane playerPlane(chosenPlaneModel); //create plane object based on choosen plane
+        playerPlane.choosePlane(); //output plane statistics
+        
+        std::string chosenCoPilotName;
+        // Check if the chosen plane requires a co-pilot
+        if (playerPlane.getRequiresCoPilot()) {
+            // Let the player choose their co-pilot
+            while (true) {
+                std::cout << "Enter the name of your Co-Pilot: (Goose, Bob) \n Co-Pilot: ";
+                std::cin >> chosenCoPilotName;
+                chosenCoPilotName[0] = toupper(chosenCoPilotName[0]); // Convert the first letter to uppercase
+                
+                // Check if the entered co-pilot name is valid
+                std::string lowerCaseCoPilotName = toLowerCase(chosenCoPilotName);
+                if (lowerCaseCoPilotName == "goose" || lowerCaseCoPilotName == "bob") {
+                    break; // Valid input, exit the loop
+                }
+                else {
+                    std::cout << "Invalid co-pilot name. Please choose from Goose or Bob.\n";
+                }
+            }
+        }
+        else {
+            // Create a dummy CoPilot instance with all values set to 0
+            chosenCoPilotName = "Dummy";
+        }
+        
+        CoPilot playerCoPilot(chosenCoPilotName); //create CoPilot object
+        if (chosenCoPilotName != "Dummy") {
+            playerCoPilot.chooseCoPilot(); //output CoPilot stats
+        }
+        
+        directions();
         RacingGame racingGame;
-        racingGame.simulateRace(playerPilot, playerCoPilot, playerPlane);
+        scores.push_back(racingGame.simulateRace(playerPilot, playerCoPilot, playerPlane));
+        std::cout << "Personal best time: " << *std::min_element(scores.begin(),scores.end()) << std::endl;
+        
+        int again;
+        std::cout << "Play again? (Enter 0 for no and 1 for yes)" << std::endl;
+        std::cin >> again;
+        while (again != 0 && again != 1) {
+            std::cout << "Invalid input" << std::endl;
+            std::cout << "Play again? (Enter 0 for no and 1 for yes)" << std::endl;
+            std::cin >> again;
+        }
+        if (again == 0) {
+            break;
+        }
     }
-    else {
-        // Create a dummy CoPilot instance with all values set to 0
-        CoPilot dummyCoPilot("Dummy");
-        dummyCoPilot.chooseCoPilot();
-
-        // Create an instance of RacingGame and start the game with pilot and plane
-        RacingGame racingGame;
-        racingGame.simulateRace(playerPilot, dummyCoPilot, playerPlane);
-    }
-
+    
     return 0;
 }
-
-
-
