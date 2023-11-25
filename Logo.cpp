154d6c78fd6392fb9f41e09e2a51bfa24c6ca695
AGlenn357 <143755656+AGlenn357@users.noreply.github.com>
2023-11-24 22:30:30 -0500

    Create Logo.cpp

---
+++ Logo.cpp
@@ -0,0 +1,17 @@
+//
+//  Logo.cpp
+//  final project
+//
+//  Created by Andrew Glenn on 11/24/23.
+//
+
+#include <iostream>
+#include "Logo.h"
+
+void read_logo() {  // main logo displayed at game's start
+    std::cout << "#######  ####   ####    ####   ##  ##   ###   # \n";
+    std::cout << "   #    #    #  #   #  #       ##  ##   ####  #    \n";
+    std::cout << "   #    #    #  ####   #  # #  ##  ##   ##  # # \n";
+    std::cout << "   #     ####   #       ####    ####    ##   ## \n";
+    std::cout << " \n              THE ULTIMATE RACE\n";
+}
